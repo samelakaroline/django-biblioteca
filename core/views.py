5764476bc6d4fa1d9af52b197ab0dceaad8145b4
samelakaroline <37110801+samelakaroline@users.noreply.github.com>
2024-10-08 22:47:12 -0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,45 +1,41 @@
-from rest_framework import generics
-from django_filters import rest_framework as filters
+from rest_framework import generics 
 from .models import Livro, Categoria, Autor
 from .serializers import LivroSerializer, CategoriaSerializer, AutorSerializer
-from .filters import LivroFilter
+from .filters import LivroFilter, CategoriaFilter, AutorFilter
 
-# View para listar e criar livros com filtros, paginação e ordenação
+
 class LivroList(generics.ListCreateAPIView):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
     filterset_class = LivroFilter
-    ordering_fields = ['titulo', 'autor', 'categoria', 'publicado_em']
-    name = "livro-list"
+    ordering_fields = ['titulo', 'autor', 'categoria', 'publicado_em'] 
+    name = 'livro-list'
 
-# View para obter, atualizar ou deletar um livro específico
 class LivroDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
-    name = "livro-detail"
-
-
-# View para listar e criar categorias
+    name = 'livro-detail'
+    
 class CategoriaList(generics.ListCreateAPIView):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
-    name = "categoria-list"
-
-# View para obter, atualizar ou deletar uma categoria específica
+    filterset_class = CategoriaFilter
+    ordering_fields = ['nome']
+    name = 'categoria-list'
+    
 class CategoriaDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
-    name = "categoria-detail"
-
-
-# View para listar e criar autores
+    name = 'categoria-detail'
+    
 class AutorList(generics.ListCreateAPIView):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializer
-    name = "autor-list"
-
-# View para obter, atualizar ou deletar um autor específico
+    filterset_class = AutorFilter
+    ordering_fields = ['nome']
+    name = 'autor-list'
+    
 class AutorDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializer
-    name = "autor-detail"
+    name = 'autor-detail' 
